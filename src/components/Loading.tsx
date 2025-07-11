5c44da44410822be98695e57c6c2beb652244532
Eniola Samuel <theeniolasamuel@gmail.com>
2025-07-11 15:10:40 +0100

    apartment filters update

--- src/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex justify-center items-center py-12">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-teal-500"></div>
-        </div>
-    )
-}
